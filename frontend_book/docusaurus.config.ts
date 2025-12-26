49cd8fd9d78b138e1d38a4f84b84adfdc06e33da
Rabia Mashkoor <163205651+Rabia0125@users.noreply.github.com>
2025-12-26 17:38:04 +0500

    this is my first hackathon-1 project

--- frontend_book/docusaurus.config.ts
+++ frontend_book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://rabia-hackathon-1-book.vercel.app',
+  url: 'https://your-username.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/frontend_book/',
